f233e3a26a4c97f9fe8fb3462e3bc03ab16a76e6
Maxime Vaillancourt <mv@maximevaillancourt.com>
2020-03-14 19:52:40 -0400

    Create 'User' and 'Article' models

---
+++ app/models/article.rb
@@ -0,0 +1,3 @@
+class Article < ApplicationRecord
+  belongs_to :user
+end
---
+++ app/models/user.rb
@@ -0,0 +1,26 @@
+require 'securerandom'
+
+class User < ApplicationRecord
+  has_many :articles, dependent: :destroy
+  validates :account_number, uniqueness: true, presence: true
+
+  def self.build
+    user = new
+    user.account_number = generate_account_number
+    user.save
+  end
+
+  private_class_method def self.generate_account_number
+    new_account_number = generate_16_digit_number
+
+    while find_by(account_number: new_account_number)
+      new_account_number = generate_16_digit_number
+    end
+
+    new_account_number
+  end
+
+  private_class_method def self.generate_16_digit_number
+    SecureRandom.random_number(10**16).to_s.rjust(16, '0')
+  end
+end
