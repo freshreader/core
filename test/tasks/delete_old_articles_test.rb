5d89e4ebf10c08c5c8c795e576a48a46956817c8
Maxime Vaillancourt <mv@maximevaillancourt.com>
2020-03-15 11:15:09 -0400

    Add rake task to delete articles created more than 14 days ago (#9)

---
+++ test/tasks/delete_old_articles_test.rb
@@ -0,0 +1,28 @@
+require 'test_helper'
+require 'rake'
+
+class DeleteOldArticlesTest < ActiveSupport::TestCase
+  def setup
+    Freshreader::Application.load_tasks if Rake::Task.tasks.empty?
+
+    @user = User.new(account_number: '1234123412341234')
+    @user.save
+  end
+
+  def teardown
+    @user.destroy
+  end
+
+  test "task deletes articles created more than 14 days ago" do
+    @article_created_15_days_ago = Article.new(url: 'foo', user: @user, created_at: 15.days.ago)
+    assert @article_created_15_days_ago.save
+    @article_created_13_days_ago = Article.new(url: 'bar', user: @user, created_at: 13.days.ago)
+    assert @article_created_13_days_ago.save
+
+    Rake::Task["delete_old_articles"].invoke
+
+    remaining_articles = ::Article.all
+    refute_includes remaining_articles, @article_created_15_days_ago
+    assert_includes remaining_articles, @article_created_13_days_ago
+  end
+end
