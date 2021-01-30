c44fefa4f6dadcd6ec60a2df17d52ceb56c49ee2
Maxime Vaillancourt <mv@maximevaillancourt.com>
2021-01-30 15:14:28 -0500

    Fix broken test

--- test/helpers/request_helper_test.rb
+++ test/helpers/request_helper_test.rb
@@ -45,7 +45,7 @@ class RequestHelperTest < ActiveSupport::TestCase
   end
 
   test '.extract_title_from_page falls back to http if necessary' do
-    expected_title = "NeverSSL - Connecting ..."
+    expected_title = "NeverSSL - helping you get online"
     expected_uri = "http://neverssl.com/"
 
     actual_title, actual_uri = RequestHelper.extract_title_from_page('http://neverssl.com/')
